a654dc910703cd138720eca1707b6ded4554c1d2
akozlovski97 <60106043+akozlovski97@users.noreply.github.com>
2022-04-29 13:26:58 +0300

    Update Biotop.py

--- Biotop.py
+++ Biotop.py
@@ -10,5 +10,6 @@ mes = "Биотоп"
 class BioMod(loader.Module):
 	strings = {"name": "BioMod"}
 	async def ruscmd(self, message):
-    while(1==1):
-		await message.edit(idp, mes)
+		while(1==1):
+			await message.edit(idp, mes)
+			await sleep(7)
