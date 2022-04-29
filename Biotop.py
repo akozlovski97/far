c97ce35cec68f807de3cae62ed6dcc15e4de5001
akozlovski97 <60106043+akozlovski97@users.noreply.github.com>
2022-04-29 13:22:45 +0300

    Create Biotop.py

---
+++ Biotop.py
@@ -0,0 +1,14 @@
+from .. import loader
+import telethon
+import time
+import asyncio 
+
+px = int(2)
+idp = "@ajshsuwj"
+mes = "Биотоп"
+
+class BioMod(loader.Module):
+	strings = {"name": "BioMod"}
+	async def ruscmd(self, message):
+    while(1==1):
+		await message.edit(idp, mes)
