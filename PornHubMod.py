7602be0b385dca189476bd0bf2565fcdd53e66ae
akozlovski97 <60106043+akozlovski97@users.noreply.github.com>
2022-05-02 15:26:58 +0300

    Update and rename P.py to PornHubMod.py

---
+++ PornHubMod.py
@@ -0,0 +1,26 @@
+from random import choice
+
+from pornhub_api import PornhubApi
+from telethon import types
+
+from .. import loader, utils
+
+
+@loader.tds
+class PhSrchMod(loader.Module):
+    strings = {"name": "PornHubMod"}
+
+    @loader.owner
+    async def sphcmd(self, m: types.Message):
+        "Найти видео на pornhub"
+        if args := utils.get_args_raw(m):
+            srch = args
+        else:
+            return await m.delete()
+        api = PornhubApi()
+        data = api.search.search(srch, ordering="mostviewed")
+        video = choice(data.videos)
+        await utils.answer(
+            m,
+            f'<b>Нашёл кое-что по запросу</b> <code>{srch}</code>: <a href="{video.url}">{video.title}</a>',
+        )
