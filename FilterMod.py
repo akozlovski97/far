26a8177e8022916b7ba6411fdedb3f352d92711c
akozlovski97 <60106043+akozlovski97@users.noreply.github.com>
2022-07-02 01:22:33 +0300

    Update and rename Filter.py to FilterMod.py

--- FilterMod.py
+++ FilterMod.py
@@ -2,9 +2,9 @@ from .. import loader, utils
 
 
 @loader.tds
-class FiltersMod(loader.Module):
-    """Фильтры"""
-    strings = {"name": "Filters"}
+class FilterMod(loader.Module):
+    """Добавляет фильтры в чат"""
+    strings = {"name": "FilterMod"}
 
     async def client_ready(self, client, db):
         self.db = db
@@ -17,29 +17,29 @@ class FiltersMod(loader.Module):
         chatid = str(message.chat_id)
 
         if not key and not reply:
-            return await message.edit("<b>Нет аргументов и реплая.</b>")
+            return await message.edit("<b>Нет аргументов и реплая❗️</b>")
 
         if chatid not in filters:
             filters.setdefault(chatid, {})
 
         if key in filters[chatid]:
-            return await message.edit("<b>Такой фильтр уже есть.</b>")
+            return await message.edit("<b>Такой фильтр уже есть❗️</b>")
 
         if reply:
             if key:
                 msgid = await self.db.store_asset(reply)
             else:
-                return await message.edit("<b>Нужны аргументы, чтобы сохранить фильтр!</b>")
+                return await message.edit("<b>Нужны аргументы, чтобы сохранить фильтр❗️</b>")
         else:
             try:
                 msgid = (await message.client.send_message(f'friendly-{(await message.client.get_me()).id}-assets', key.split(' / ')[1])).id
                 key = key.split(' / ')[0]
             except IndexError:
-                return await message.edit("<b>Нужен второй аргумент (через / )или реплай.</b>")
+                return await message.edit("<b>Нужен второй аргумент (через / )или реплай❗️</b>")
 
         filters[chatid].setdefault(key, msgid)
         self.db.set("Filters", "filters", filters)
-        await message.edit(f"<b>Фильтр \"{key}\" сохранён!</b>") 
+        await message.edit(f"<b>✅ Фильтр \"{key}\" сохранён!</b>") 
 
 
     async def stopcmd(self, message):
@@ -49,20 +49,20 @@ class FiltersMod(loader.Module):
         chatid = str(message.chat_id)
 
         if chatid not in filters:
-            return await message.edit("<b>В этом чате нет фильтров.</b>")
+            return await message.edit("<b>В этом чате нет фильтров❗️</b>")
 
         if not args:
-            return await message.edit("<b>Нет аргументов.</b>")
+            return await message.edit("<b>Нет аргументов❗️</b>")
 
         if args:
             try:
                 filters[chatid].pop(args)
                 self.db.set("Filters", "filters", filters)
-                await message.edit(f"<b>Фильтр \"{args}\" удалён из чата!</b>")
+                await message.edit(f"<b>✅ Фильтр \"{args}\" удалён из чата!</b>")
             except KeyError:
-                return await message.edit(f"<b>Фильтра \"{args}\" нет.</b>")
+                return await message.edit(f"<b>Фильтра \"{args}\" нет❗️</b>")
         else:
-            return await message.edit("<b>Нет аргументов.</b>")
+            return await message.edit("<b>Нет аргументов❗️</b>")
 
 
     async def stopallcmd(self, message):
@@ -71,11 +71,11 @@ class FiltersMod(loader.Module):
         chatid = str(message.chat_id)
  
         if chatid not in filters:
-            return await message.edit("<b>В этом чате нет фильтров.</b>")
+            return await message.edit("<b>В этом чате нет фильтров❗️</b>")
 
         filters.pop(chatid)
         self.db.set("Filters", "filters", filters)
-        await message.edit("<b>Всё фильтры были удалены из списка чата!</b>")
+        await message.edit("<b>✅ Всё фильтры были удалены из списка чата!</b>")
 
 
     async def filterscmd(self, message):
@@ -84,12 +84,12 @@ class FiltersMod(loader.Module):
         chatid = str(message.chat_id)
 
         if chatid not in filters:
-            return await message.edit("<b>В этом чате нет фильтров.</b>")
+            return await message.edit("<b>В этом чате нет фильтров❗️</b>")
 
         msg = ""
         for _ in filters[chatid]:
             msg += f"<b>• {_}</b>\n"
-        await message.edit(f"<b>Список фильтров в этом чате: {len(filters[chatid])}\n\n{msg}</b>") 
+        await message.edit(f"<b>📋 Список фильтров в этом чате: {len(filters[chatid])}\n\n{msg}</b>") 
 
 
     async def watcher(self, message):
