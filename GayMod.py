f1777fffbe3bb6898e7b0d47275a311d63dabd3d
akozlovski97 <60106043+akozlovski97@users.noreply.github.com>
2022-07-19 23:25:44 +0300

    Create GayMod.py

---
+++ GayMod.py
@@ -0,0 +1,12 @@
+import random
+from .. import utils, loader
+import asyncio, pytz, re, telethon
+
+@loader.tds
+class GayMod(loader.Module):
+    """Узнай на сколько ты гей!"""
+    strings = {"name": "GayMod"}
+    
+    def gaycmd(self, message):
+        """Покажет на сколько ты гей"""
+            await message.respond("<b>Ты гей на<b\> " + str(random.randrange(0, 101)) + "%")
