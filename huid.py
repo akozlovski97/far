4313f717439602e08d5144f39e4354c895a80909
akozlovski97 <60106043+akozlovski97@users.noreply.github.com>
2022-09-10 15:53:30 +0300

    Update huid.py

--- huid.py
+++ huid.py
@@ -107,6 +107,7 @@ class NumMod(loader.Module):
 					await message.reply('<b>Ты ввел хуйню⁉️</b>')
 			except:
 				await message.reply("Заразить " + reply.raw_text[json["entities"][i]["offset"]:json["entities"][i]["offset"]+json["entities"][i]["length"]] )
+			await asyncio.sleep(3)
 		
 	async def exnumcmd(self, message):
 		"Добавляет исключения в модуль.\nИспользуй:  .exnum (@id/user)"
