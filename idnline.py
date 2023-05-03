dacc7f0f9cdc7a8173d6c23da132af1b33edab50
akozlovski97 <60106043+akozlovski97@users.noreply.github.com>
2023-05-04 01:22:03 +0300

    Create idnline.py

---
+++ idnline.py
@@ -0,0 +1,24 @@
+import asyncio
+from telethon import Button
+from .. import utils, loader
+
+class InlMod(loader.Module):
+	"""инлайн"""
+	strings = {"name": "GayMod"}
+    
+	def inlcmd(self, message):
+		"""Нажимает"""
+
+		reply = await event.get_reply_message()
+		inline_keyboard = reply.reply_markup
+		buttons = await reply.get_buttons()
+		button = buttons[1][0]
+		await button.click()
+
+		await asyncio.sleep(3)
+
+		reply = await event.get_reply_message()
+		new_inline_keyboard = reply.reply_markup
+		buttons = await reply.get_buttons()
+		button = buttons[2][0]
+		await button.click()
