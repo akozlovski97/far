8bf9102187708735f727502eb46814d5ea977683
akozlovski97 <60106043+akozlovski97@users.noreply.github.com>
2023-05-04 01:24:23 +0300

    Update idnline.py

--- idnline.py
+++ idnline.py
@@ -8,15 +8,12 @@ class InlMod(loader.Module):
     
 	def inlcmd(self, message):
 		"""Нажимает"""
-
 		reply = await event.get_reply_message()
 		inline_keyboard = reply.reply_markup
 		buttons = await reply.get_buttons()
 		button = buttons[1][0]
 		await button.click()
-
 		await asyncio.sleep(3)
-
 		reply = await event.get_reply_message()
 		new_inline_keyboard = reply.reply_markup
 		buttons = await reply.get_buttons()
